5b9935e4a5944945083b1cfceb6bf33f93b3eb0d
akash1kumar20 <akashkumar2101sm@gmail.com>
2025-11-16 00:09:52 +0530

    feat : Calling weather API

--- script.js
+++ script.js
@@ -18,3 +18,41 @@ function toggleTheme() {
   localStorage.setItem("theme", JSON.stringify(currentTheme));
 }
 applyTheme();
+
+//weattherApiCalling
+const apiKey = "1a37e5386d52f2e853aed05d97d1595c";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric";
+let city = "";
+let degree = "°C";
+
+async function checkWeather() {
+  city = "meerut";
+  document.querySelector(".loader").style.display = "block";
+  try {
+    let res = await fetch(apiUrl + `&q=${city}` + `&appid=${apiKey}`);
+    let data = await res.json();
+    console.log(data);
+    document.querySelector(".temperature").innerHTML =
+      Math.round(data.main.temp) + ` ${degree}`;
+    document.querySelector(".city-name").innerHTML = data.name;
+    document.querySelector(".country").innerHTML = data.sys.country;
+    document.querySelector(".description").innerHTML =
+      data.weather[0].description.toUpperCase();
+    document.querySelector("#humidity").innerHTML = data.main.humidity;
+    document.querySelector("#wind").innerHTML = data.wind.speed;
+    document.getElementById("feels-like").innerHTML = data.main.feels_like;
+    let sunrise = data.sys.sunrise;
+    document.getElementById("sunrise").innerHTML = new Date(
+      sunrise * 1000
+    ).toLocaleTimeString();
+    let sunset = data.sys.sunset;
+    document.getElementById("sunset").innerHTML = new Date(
+      sunset * 1000
+    ).toLocaleTimeString();
+    document.querySelector(".loader").style.display = "none";
+  } catch (err) {
+    document.querySelector(".hidden").style.visible = "block";
+  }
+}
+
+checkWeather();
